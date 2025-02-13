b3c2df196740fbdca29e47c6b14b2ca7ba4ee9a8
nanda kresna <habel180803@gmail.com>
2025-02-13 18:10:26 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,48 @@
 /* ini file Javascript */
 
 function KonversiSuhu() {
-  // Mendapatkan nilai input Celsius
+  // Mengambil nilai input suhu dalam Celsius
   let celsius = document.getElementById("main-input").value;
 
-  // Validasi input: pastikan input tidak kosong dan berupa angka
+  // Validasi input
   if (celsius === "" || isNaN(celsius)) {
-      alert("Masukkan suhu dalam angka.");
-      return;
+    alert("Masukkan suhu dalam angka!");
+    return;
   }
 
-  // Melakukan konversi Celsius ke Fahrenheit
-  let fahrenheit = (celsius * 9 / 5) + 32;
+  // Mengkonversi Celsius ke Fahrenheit
+  let fahrenheit = (celsius * 9/5) + 32;
 
-  // Menampilkan hasil konversi pada input Fahrenheit
+  // Menampilkan hasil konversi
   document.getElementById("main-result").value = fahrenheit;
 
-  // Menampilkan cara konversi pada input cara konversi
-  document.getElementById("cara-konversi").value = "(C x 9/5) + 32";
+  // Menampilkan cara konversi
+  document.getElementById("cara-konversi").value = `(${celsius} * 9/5) + 32`;
 }
 
 function resetForm() {
-  // Mereset nilai input Celsius dan Fahrenheit
+  // Mereset nilai input dan output
   document.getElementById("main-input").value = "";
   document.getElementById("main-result").value = "";
   document.getElementById("cara-konversi").value = "";
 }
 
 function reverseKonversi() {
-  // Mendapatkan nilai input Celsius dan Fahrenheit
-  let celsius = document.getElementById("main-input").value;
-  let fahrenheit = document.getElementById("main-result").value;
+  // Mengambil nilai input suhu dalam Fahrenheit
+  let fahrenheit = document.getElementById("main-input").value;
 
-   // Validasi input: pastikan input tidak kosong dan berupa angka
-  if (celsius === "" || isNaN(celsius) || fahrenheit === "" || isNaN(fahrenheit)) {
-      alert("Masukkan suhu dalam angka.");
-      return;
+   // Validasi input
+  if (fahrenheit === "" || isNaN(fahrenheit)) {
+    alert("Masukkan suhu dalam angka!");
+    return;
   }
 
-  // Melakukan konversi Fahrenheit ke Celsius
-  let celsiusBaru = (fahrenheit * 1.8) + 32;
+  // Mengkonversi Fahrenheit ke Celsius
+  let celsius = (fahrenheit - 32) * 5/9;
 
-  // Menampilkan hasil konversi pada input Celsius
-  document.getElementById("main-input").value = celsiusBaru;
-  document.getElementById("main-result").value = fahrenheit;
+  // Menampilkan hasil konversi
+  document.getElementById("main-result").value = celsius;
 
-  // Menampilkan cara konversi pada input cara konversi
-  document.getElementById("cara-konversi").value = "(F * 1.8) + 32";
+  // Menampilkan cara konversi
+  document.getElementById("cara-konversi").value = `(${fahrenheit} - 32) * 5/9`;
 }
